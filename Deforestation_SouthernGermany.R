cf867dd06bfa2bf7fc8cfe567af828d590c80bc2
LuciaTi <lucia.tischer@gmx.de>
2018-02-14 13:01:39 +0100

    First Scipt-Structure for the Analysis of Deforestation in Souther German Montane Forest

---
+++ Deforestation_SouthernGermany.R
@@ -0,0 +1,41 @@
+##########################################################
+## Deforestation of montane forests in southern Germany ##
+##########################################################
+
+
+## Theme Description:
+
+
+## Chosen Data:
+
+
+## Used Libraries:
+
+
+
+
+## 1) Reading in the Data, ropping and first plotting ####
+
+
+## 2) Control for Clouds ####
+
+
+## 3) Defining elevation zonation ####
+
+
+## 4) Landcover Classifications and Productivity of single scenes ####
+
+
+## 5) Comparison of Landcover ####
+
+
+## 6) Comparison of Productivity ####
+
+
+## 7) Productivity-Change of deforested areas ####
+
+
+## 8) Productivity-Change of deforested areas of different elevation ####
+
+
+
